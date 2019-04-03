9b1445182d2b17dac92c65b99c0c7d27d64c51d6
Arif <44020819+azimarif@users.noreply.github.com>
2019-04-03 10:01:55 +0530

    Inital Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import nextGeneration from './gameOfLife'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class GameOfLife extends React.Component {
+  constructor(props) {
+    super(props);
+    this.initialize();
+    this.bounds = { topLeft: [0, 0], bottomRight: [this.props.size, this.props.size] };
+    this.state = {
+      aliveCells: []
+    }
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  initialize() {
+    this.initializeAliveCell = this.initializeAliveCell.bind(this);
+    this.start = this.start.bind(this);
+    this.nextGeneration = nextGeneration.bind(this);
+    this.displayNextGeneration = this.displayNextGeneration.bind(this);
+  }
+
+  start() {
+    setInterval(this.displayNextGeneration, 500);
+  }
+
+  displayNextGeneration() {
+    this.state.aliveCells.map(cell =>
+      document.getElementById(cell.join('_')).className = 'cell');
+
+    let nextGeneration = this.nextGeneration(this.state.aliveCells, this.bounds);
+
+    nextGeneration.map(cell =>
+      document.getElementById(cell.join('_')).className = 'active-cell');
+    this.setState({ aliveCells: nextGeneration })
+  }
+
+  initializeAliveCell(event) {
+    const selectedCell = event.target.id;
+    const cell = selectedCell.split('_');
+    document.getElementById(selectedCell).className = 'active-cell';
+    let aliveCells = this.state.aliveCells;
+    aliveCells.push(cell);
+    this.setState({ aliveCells });
+  }
+
+  createRow(rowIndex) {
+    let columns = [];
+    for (let column = 0; column < this.props.size; column++) {
+      const id = rowIndex + '_' + column;
+      columns.push(<td className='cell' key={id} id={id} onClick={this.initializeAliveCell} />
+      );
+    }
+    return columns;
+  }
+
+  renderTable() {
+    let table = [];
+    for (let row = 0; row < this.props.size; row++) {
+      table.push(<tr key={row}>{this.createRow(row)}</tr>);
+    }
+    return table;
+  }
+
+  render() {
+    return (
+      <div>
+        <table>{this.renderTable()}</table>
+        <button onClick={this.start}>Start</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<GameOfLife size={15} />, document.getElementById('root'));
+
