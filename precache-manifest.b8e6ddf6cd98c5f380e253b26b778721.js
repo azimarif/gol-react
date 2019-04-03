c87fc7359bdda65f5676f3f2e12b4f737a036158
Arif <44020819+azimarif@users.noreply.github.com>
2019-04-03 10:06:14 +0530

    Updates

---
+++ precache-manifest.b8e6ddf6cd98c5f380e253b26b778721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d99d7a9e223364735716",
+    "url": "/gol-react/static/js/runtime~main.b14f0b41.js"
+  },
+  {
+    "revision": "b4a3e8bb36e8a828e7ce",
+    "url": "/gol-react/static/js/main.e7c89a0c.chunk.js"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/gol-react/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "b4a3e8bb36e8a828e7ce",
+    "url": "/gol-react/static/css/main.096d9ae5.chunk.css"
+  },
+  {
+    "revision": "1c158487d16f64b8282515701bfc95ab",
+    "url": "/gol-react/index.html"
+  }
+];
